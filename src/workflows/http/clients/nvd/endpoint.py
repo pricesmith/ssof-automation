45273e1973b65e31e214c23573a4b3eca089f4f5
Price Smith <psmith.code@gmail.com>
2022-10-04 14:48:50 -0700

    update nvd client

--- src/workflows/http/clients/nvd/endpoint.py
+++ src/workflows/http/clients/nvd/endpoint.py
@@ -53,14 +53,75 @@ class NVD(object):
 
     class CVE(object):
         def __init__(self):
-            self.q = f'{CPES_BASE_URL}{DEFAULT_VERSION}'
+            self.query = f'{CPES_BASE_URL}{DEFAULT_VERSION}'
 
-            self.cpe_name                   = 'cpeName'
-            self.cve_id                     = 'cveId'
-            self.cpe_keyword_search         = 'keywordSearch'
-            self.cpe_keyword_exact_match    = 'keywordExactMatch'
+            self.cve_query_terms = {
+                'cpe_name'               : 'cpeName',
+                'cve_id'                 : 'cveId',
+                'cwe_id'                 : 'cweId',
+                'cvss_v2_metrics'        : 'cvssV2metrics',
+                'cvss_v3_metrics'        : 'cvssV3metrics',
+                'cvss_v2_severity'       : 'cvssV2Severity',
+                'cvss_v3_severity'       : 'cvssV3Severity',
+                'keyword_search'         : 'keywordSearch',
+                'keyword_exact_match'    : 'keywordExactMatch',
+                'has_cert_alerts'        : 'hasCertAlerts',
+                'has_cert_notes'         : 'hasCertNotes',
+                'has_kev'                : 'hasKev',
+                'has_oval'               : 'hasOval',
+                'is_vulnerable'          : 'isVulnerable',
+                'keyword_search'         : 'keywordSearch',
+                'keyword_exact_match'    : 'keywordExactMatch',
+                'last_mod_start_date'    : 'lastModStartDate',
+                'last_mod_end_date'      : 'lastModEndDate',
+                'pub_start_date'         : 'pubStartDate',
+                'pub_end_date'           : 'pubEndDate',
+                'results_per_page'       : 'resultsPerPage',
+                'start_index'            : 'startIndex',
+            }
 
-        def search()
+        def search(
+            self,
+            cpe_name: str               = None,
+            cve_id: str                 = None,
+            cwe_id: str                 = None,
+            cvss_v2_metrics: str        = None,
+            cvss_v3_metrics: str        = None,
+            cvss_v2_severity: str       = None,
+            cvss_v3_severity: str       = None,
+            has_cert_alerts: bool       = None,
+            has_cert_notes: bool        = None,
+            has_kev: bool               = None,
+            has_oval: bool              = None,
+            is_vulnerable: bool         = None, # If filtering by isVulnerable, cpeName is REQUIRED. Please note, virtualMatchString is not accepted in requests that use isVulnerable.
+            keyword_search: str         = None, # Please note, multiple {keywords} function like an 'AND' statement. This returns results where all keywords exist somewhere in the current description, though not necessarily together.
+            keyword_exact_match: bool   = None,
+            last_mod_start_date: str    = None,
+            last_mod_end_date: str      = None,
+            pub_start_date: str         = None,
+            pub_end_date: str           = None,
+            results_per_page: int       = None,
+            start_index: int            = None,
+        ):
+            kwargs = locals()
+            query_params = []
+
+            for key in kwargs:
+                if kwargs[key]:
+                    if type(kwargs[key]) is str or int:
+                        query_params.append(f'{self.cve_query_terms[key]}={kwargs[key]}')
+
+                    if type(kwargs[key]) is bool and kwargs[key] is True:
+                        query_params.append(f'{self.cve_query_terms[key]}')
+            
+            query_params_str = '?'
+            for param in query_params:
+                query_params_str += param
+
+            
+
+
+
 
         class Search(object):
             def __init__(self):
