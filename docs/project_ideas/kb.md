14e35dcf38e6662a84e5686a583566775941544c
Price Smith <psmith.code@gmail.com>
2022-10-05 14:08:44 -0700

    rdflib addition

---
+++ docs/project_ideas/kb.md
@@ -0,0 +1,9 @@
+# kb
+
+## todo
+
+- docker container for rdflib
+- dag to pull updates from ontology
+
+- if onto update, run inferences
+- for every db insert, run inferences
---
+++ src/dags/include/custom_operators/json_io_operator.py
@@ -0,0 +1,84 @@
+"""
+Supply JSON input into the Docker Container
+
+Extract file outputs (XLSX, CSV, etc) from within the Docker Container
+
+Operate on multi-worker Airflow deployments
+"""
+import os
+import json
+import tempfile
+
+from airflow.operators import DockerOperator
+
+# Inherit functionality from DockerOperator --
+# The DockerOperator takes care of supplying arguments necessary 
+# to run the container and starts up the container.
+class JsonIoOperator(DockerOperator):
+
+    def __init__(
+        self, 
+        input_task_id, 
+        shared_dir_path,
+        output_dir_path = 'tmp/output',
+        *args, 
+        **kwargs,
+    ):
+        super().__init__(*args, **kwargs)
+        self.input_task_id      = input_task_id
+        self.shared_dir_path    = shared_dir_path
+        self.output_dir_path    = output_dir_path
+        
+    def execute_to_temp_nfs(self, context):
+        # pass input logic goes here
+        input = self.xcom_pull(task_ids     = self.input_task_id, context = context)
+        self.environment['CONTAINER_INPUT'] = json.dumps(input)
+
+        # setup output logic goes here
+        self.environment['OUTPUT_DIR'] = self.output_dir_path
+        tmp_dir = tempfile.TemporaryDirectory(dir=self.shared_dir_path)
+        tmp_dir_path = tmp_dir.name
+        
+        # append volume
+        volume = "{}:{}:rw".format(tmp_dir_path, self.output_dir_path)
+        self.volumes.append(volume)
+
+        # run the container
+        super().execute(context)
+
+        # load output into Airflow logic goes here
+
+        # returns output file path
+        return tmp_dir_path
+
+    def execute_to_xcom(self, context):
+        # pass input logic goes here
+        input = self.xcom_pull(task_ids     = self.input_task_id, context = context)
+        self.environment['CONTAINER_INPUT'] = json.dumps(input)
+
+        # setup output logic goes here
+        self.environment['OUTPUT_DIR'] = self.output_dir_path
+        tmp_dir = tempfile.TemporaryDirectory(dir = self.shared_dir_path)
+        files = {}
+
+        # using context to avoid explicit garbage collection code
+        with tmp_dir as tmp_dir:
+            tmp_dir_path = tmp_dir.name
+
+            # append volume
+            volume = "{}:{}:rw".format(tmp_dir_path, self.output_dir)
+            self.volumes.append(volume)
+
+            # run the container
+            super().execute(context)
+
+            # load output into Airflow logic goes here
+
+            # extract files
+            for filename in os.listdir(tmp_dir_path):
+                filepath = os.path.join(tmp_dir_path, filename)
+                with open(filepath, 'rb') as f:
+                    files[filename] = f.read()
+
+        # returns files
+        return files
